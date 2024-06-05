8aff76a1d733e76fe45ecc02a66ba45063a2ccde
MacUser <user@macs-mac-mini.home>
2024-06-04 23:29:14 -0400

    some code added

---
+++ checklist/checklist/ChecklistViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  checklist
+//
+//  Created by MacUser on 2024-06-04.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
